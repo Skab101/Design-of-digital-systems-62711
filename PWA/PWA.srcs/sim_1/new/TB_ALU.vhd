d1f46b4305ad9a238a88aba3fc52876720f2e02c
Jonas Beck Jensen <jonasbjensen@outlook.dk>
2026-03-02 14:48:03 +0100

    TB ALU

---
+++ PWA/PWA.srcs/sim_1/new/TB_ALU.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02.03.2026 14:46:20
+-- Design Name: 
+-- Module Name: TB_ALU - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TB_ALU is
+--  Port ( );
+end TB_ALU;
+
+architecture Behavioral of TB_ALU is
+
+begin
+
+
+end Behavioral;
