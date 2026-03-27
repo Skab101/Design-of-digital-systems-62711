7d33b3c5c7330d6d706803aebd8093253bd0ad94
AndreasJacobsen <s241123@dtu.dk>
2026-03-27 12:51:02 +0100

    PC til test

--- PWB/sources/hdl/ProgramCounter.vhd
+++ PWB/sources/hdl/ProgramCounter.vhd
@@ -6,33 +6,26 @@ entity ProgramCounter is
     port (
         RESET      : in  STD_LOGIC;
         CLK        : in  STD_LOGIC;
-        Load       : in  STD_LOGIC;
-        Count      : in  STD_LOGIC;
         Address_In : in  STD_LOGIC_VECTOR(7 downto 0);
         PS         : in  STD_LOGIC_VECTOR(1 downto 0);
         Offset     : in  STD_LOGIC_VECTOR(7 downto 0);
-        D          : in  STD_LOGIC_VECTOR(7 downto 0);
         CarryO     : out STD_LOGIC;
-        Q          : out STD_LOGIC_VECTOR(7 downto 0);
         PC         : out STD_LOGIC_VECTOR(7 downto 0)
     );
 end ProgramCounter;
 
 architecture PC_Structural of ProgramCounter is
 
-signal MUXP, CO, Cin, sum: STD_LOGIC_VECTOR(7 downto 0);
+signal MUXP, CO, sumOffset: STD_LOGIC_VECTOR(7 downto 0); 
+signal Load, Count: STD_LOGIC;
     begin
 
-    --------------------------------------------------------------------------
-    -- Gammelt kombinatorisk design (WIP fra Andreas/Jonas)
-    -- Beholdt som reference — skal rettes før det kan bruges
-    --------------------------------------------------------------------------
 
     full_adder: entity work.full_adder_8_bit
     port map(
              A    => PC,
              B    => Offset,
-             sum  => sum,
+             sum  => sumOffset,
              Cin  => '0'
      );
 
@@ -43,9 +36,9 @@ signal MUXP, CO, Cin, sum: STD_LOGIC_VECTOR(7 downto 0);
     CLK     => CLK,
     Load    => Load,
     LoadIn  => NOT Load,
-    D       => D(0),
+    D       => MUXP(0),
     Cin     => NOT Load AND Count, 
-    Q       => Q(0),
+    Q       => PC(0),
     CO      => CO(0)
     );
     
@@ -56,9 +49,9 @@ signal MUXP, CO, Cin, sum: STD_LOGIC_VECTOR(7 downto 0);
     CLK     => CLK,
     Load    => Load,
     LoadIn  => NOT Load,
-    D       => D(1),
-    Cin     => Cin(1),
-    Q       => Q(1),
+    D       => MUXP(1),
+    Cin     => CO(0),
+    Q       => PC(1),
     CO      => CO(1)
     );
     
@@ -69,9 +62,9 @@ signal MUXP, CO, Cin, sum: STD_LOGIC_VECTOR(7 downto 0);
     CLK     => CLK,
     Load    => Load,
     LoadIn  => NOT Load,
-    D       => D(2),
-    Cin     => Cin(2),
-    Q       => Q(2),
+    D       => MUXP(2),
+    Cin     => CO(1),
+    Q       => PC(2),
     CO      => CO(2)
     );
     
@@ -82,9 +75,9 @@ signal MUXP, CO, Cin, sum: STD_LOGIC_VECTOR(7 downto 0);
     CLK     => CLK,
     Load    => Load,
     LoadIn  => NOT Load,
-    D       => D(3),
-    Cin     => Cin(3),
-    Q       => Q(3),
+    D       => MUXP(3),
+    Cin     => CO(2),
+    Q       => PC(3),
     CO      => CO(3)
     );
 
@@ -95,9 +88,9 @@ signal MUXP, CO, Cin, sum: STD_LOGIC_VECTOR(7 downto 0);
     CLK     => CLK,
     Load    => Load,
     LoadIn  => NOT Load,
-    D       => D(4),
-    Cin     => Cin(4),
-    Q       => Q(4),
+    D       => MUXP(4),
+    Cin     => CO(3),
+    Q       => PC(4),
     CO      => CO(4)
     );
     
@@ -108,9 +101,9 @@ signal MUXP, CO, Cin, sum: STD_LOGIC_VECTOR(7 downto 0);
     CLK     => CLK,
     Load    => Load,
     LoadIn  => NOT Load,
-    D       => D(5),
-    Cin     => Cin(5),
-    Q       => Q(5),
+    D       => MUXP(5),
+    Cin     => CO(4),
+    Q       => PC(5),
     CO      => CO(5)
     );
     
@@ -121,9 +114,9 @@ Counterlogic6: entity work.CounterLogic
     CLK     => CLK,
     Load    => Load,
     LoadIn  => NOT Load,
-    D       => D(6),
-    Cin     => Cin(6),
-    Q       => Q(6),
+    D       => MUXP(6),
+    Cin     => CO(5),
+    Q       => PC(6),
     CO      => CO(6)
     );
 
@@ -134,23 +127,19 @@ Counterlogic7: entity work.CounterLogic
     CLK     => CLK,
     Load    => Load,
     LoadIn  => NOT Load,
-    D       => D(7),
-    Cin     => Cin(7),
-    Q       => Q(7),
+    D       => MUXP(7),
+    Cin     => CO(6),
+    Q       => PC(7),
     CO      => CarryO
     );
 
     
-   -- MUXP <=   NOT PS(1)=>(7 downto 0)   AND      PS(0)=>(7 downto 0) AND "0x01"  OR
-     --             PS(1)=>(7 downto 0)   AND  NOT PS(0)=>(7 downto 0) AND Offset;
+    MUXP <=       PS(1)=>(7 downto 0)   AND  NOT PS(0)=>(7 downto 0) AND Address_In  OR
+                  PS(1)=>(7 downto 0)   AND      PS(0)=>(7 downto 0) AND sumOffset;
     
 
-    PC  <=          ((NOT PS(1) AND NOT PS(0)) AND PC)         OR
-                    ((NOT PS(1) AND     PS(0)) AND sum)        OR
-                    ((    PS(1) AND NOT PS(0)) AND sum)        OR
-                    ((    PS(1) AND     PS(0)) AND Address_In);
+    Load  <=     (PS(1) AND PS(0)) OR (PS(1) AND NOT PS(0)); 
+    Count <= NOT  PS(1) AND PS(0); 
 
 
-            
-
-end PC_Structural;
+end PC_Structural; 
